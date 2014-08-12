8095a226f80840bed3624183e2df2662b977f921
postazure <gordonmaxc@gmail.com>
2014-08-12 11:10:09 -0700

    upload

--- MultiButtonTest/MultiButtonTest.ino
+++ MultiButtonTest/MultiButtonTest.ino
@@ -1,7 +1,16 @@
 
-const int button1 = 3;
-const int button2 = 4;
-const int button3 = 5;
+const int button1 = 8;
+const int button2 = 3;
+const int button3 = 2;
+
+int pinState_cur = 0;
+
+//Debounce Vars
+const int debounceTime = 300;  //300ms pause
+int timeSinceBtn_pressed = 0;
+int timeSinceBtn_counter = 0;
+int timePassed;
+
 
 void setup(){
   pinMode(button1, INPUT);
@@ -13,13 +22,33 @@ void setup(){
 
 void loop(){
 
+  
   if(digitalRead(button1) == HIGH){
-    Serial.println("BTN 1");
-  }else if(digitalRead(button2) == HIGH){
-    Serial.println("BTN 2");
-  }else if(digitalRead(button3) == HIGH){
-    Serial.println("BTN 3");
+    pinState_cur = 1;
+
   }
+  else if(digitalRead(button2) == HIGH){
+    pinState_cur = 2;
+
+  }
+  else if(digitalRead(button3) == HIGH){
+    pinState_cur = 3;
+
+  }
+  timeSinceBtn_counter = millis();
+  timePassed = timeSinceBtn_counter - timeSinceBtn_pressed;
+  
+  if (pinState_cur != 0 && timePassed > debounceTime){
+    Serial.print(timePassed);
+    Serial.print("BTN: ");
+    Serial.println(pinState_cur);
+    timeSinceBtn_pressed = timeSinceBtn_counter;
+    pinState_cur = 0;
+  }else{
+    pinState_cur = 0;
+  }
+   
 }
-  
-  
+
+
+
